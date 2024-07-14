4b5b6fa027f478f9f59dbcbe3ccbdbc6f818db5e
heminhua-he <302613511@qq.com>
2024-07-15 01:56:01 +0800

    Add 1_Sum_of_two_numbers

---
+++ 1_Sum_of_two_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int Sum_of_two_numbers()
+{
+    printf("[%s %d]\n",__FUNCTION__,__LINE__);\
+    printf("[%s %d]\n",__FUNCTION__,__LINE__);
+
+}
+
+int main() 
+{
+    printf("The first day: call Sum_of_two_numbers\n");
+    Sum_of_two_numbers();
+}
